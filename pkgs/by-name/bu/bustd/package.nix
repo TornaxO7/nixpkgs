1823aede9db8a9c1d54d82d134fa09b1383c9bcc
TornaxO7 <tornax@proton.me>
2025-01-20 17:42:59 +0100

    bustd: init at 0.1.1

---
+++ pkgs/by-name/bu/bustd/package.nix
@@ -0,0 +1,26 @@
+{ rustPlatform
+, lib
+, fetchFromGitHub
+, ...
+}: rustPlatform.buildRustPackage rec
+{
+  pname = "bustd";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "vrmiguel";
+    repo = "bustd";
+    rev = "5cf0867";
+    hash = "sha256-STyPsfJTpJqucOvoUmBYz38sZmlJCLMt81/8/8akzgQ=";
+  };
+
+  cargoHash = "sha256-Gb6DXww/9mapmLAQBe7zgjkeluxszPLfuE7/enAtA7U=";
+
+  meta = {
+    description = "Process killer daemon for out-of-memory scenarios";
+    homepage = "https://github.com/vrmiguel/bustd";
+    license = lib.licenses.mit;
+    mainProgram = pname;
+    maintainers = with lib.maintainers; [ tornax ];
+  };
+}
